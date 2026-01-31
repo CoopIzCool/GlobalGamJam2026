256226c36bfe1f07c202c135d0e0daeb627e984c
The-MEDINA <147880142+The-MEDINA@users.noreply.github.com>
2026-01-30 22:30:10 -0500

    added projection
    because of this the player now has a unique layermask

--- SheProjectMyAstral/Assets/Scripts/Player.cs
+++ SheProjectMyAstral/Assets/Scripts/Player.cs
@@ -8,9 +8,11 @@ public class Player : MonoBehaviour
     // fields
     [SerializeField] private float playerSpeed = 2;
     [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private GameObject projection;
 
-    // internal fields
+    // internal fields (things like these should only be for completing stuff within player)
     private Vector2 movementDirection;
+    private bool isProjecting = false; // should be replaced once there's a gamestate for it
 
     // Start is called before the first frame update
     void Start()
@@ -29,16 +31,46 @@ public class Player : MonoBehaviour
     /// <param name="context">Vector2 input from keyboard or gamepad.</param>
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log(context.ReadValue<Vector2>());
         movementDirection = context.ReadValue<Vector2>();
     }
 
+    public void OnProject(InputAction.CallbackContext context)
+    {
+        // toggle projection
+        if (isProjecting)
+        {
+            isProjecting = false;
+            projection.SetActive(false);
+        }
+        else
+        {
+            isProjecting = true;
+            projection.SetActive(true);
+        }
+    }
+
     /// <summary>
-    /// Moves the player around.
+    /// Moves the player and projection around.
     /// </summary>
     private void Move()
     {
-        // Move the rigidbody from its previous position.
-        rigidBody.MovePosition((Vector2) transform.position + ((movementDirection * playerSpeed) * Time.deltaTime));
+        // player movement
+        if (!isProjecting)
+        {
+            // Move the player by its rigidbody from its previous position.
+            rigidBody.MovePosition((Vector2)transform.position + ((movementDirection * playerSpeed) * Time.deltaTime));
+
+            // keep the projection and player together when not projecting
+            projection.transform.position = transform.position;
+        }
+        // projection movement
+        else
+        {
+            // grab the rigidbody to make the code cleaner
+            Rigidbody2D projectionBody = projection.GetComponent<Rigidbody2D>();
+
+            // Move the projection by its rigidbody from its previous position.
+            projectionBody.MovePosition((Vector2)projection.transform.position + ((movementDirection * playerSpeed) * Time.deltaTime));
+        }
     }
 }
