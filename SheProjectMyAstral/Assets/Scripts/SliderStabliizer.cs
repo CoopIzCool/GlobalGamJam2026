6624acd1a3ec523799f02e795ab7d3ae03e7844a
jpw3667 <jpw3667@g.rit.edu>
2026-01-31 21:20:43 -0500

    sliders fixed and main menu button

--- SheProjectMyAstral/Assets/Scripts/SliderStabliizer.cs
+++ SheProjectMyAstral/Assets/Scripts/SliderStabliizer.cs
@@ -1,33 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SliderStabllizer : MonoBehaviour
 {
-    private float master;
-    private float music;
-    private float soundfx;
+    [SerializeField] private AudioMixer audioMixer;
+    private Slider instance;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if(instance == null)
+        {
+            instance = this.gameObject.GetComponent<Slider>();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void loadVolumes()
     {
-        
-    }
+        float volume;
+        audioMixer.GetFloat(this.tag, out volume);
+        Debug.Log(volume);
+        instance.value = Mathf.Pow(10, volume/20f);
 
-    public void saveVolumes()
-    {
-        GameObject[] masterVolume = GameObject.FindGameObjectsWithTag("slider_master");
-        GameObject[] musicVolume = GameObject.FindGameObjectsWithTag("slider_music");
-        GameObject[] soundfxVolume = GameObject.FindGameObjectsWithTag("slider_soundfx");
-    }
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(this);
     }
 }
