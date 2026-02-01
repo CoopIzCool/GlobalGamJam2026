12ac5cf77af6852adfe45d3b9ddd6924a3a83da5
Stirfry92 <saw4582@g.rit.edu>
2026-01-31 22:58:43 -0500

    Animations work for regular guards.
    Hark! The Angels rejoice!

--- SheProjectMyAstral/Assets/Scripts/AnimationScripts/GuardAnimation.cs
+++ SheProjectMyAstral/Assets/Scripts/AnimationScripts/GuardAnimation.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls animation, and applies it to the enemy's location.
+/// </summary>
 public class GuardAnimation : MonoBehaviour
 {
     [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer sr;
     [SerializeField] PatrollingComponent patrollingComponent;
+    [SerializeField] GameObject animationHolder;
+    [SerializeField] GameObject enemy;
 
     private void Start()
     {
@@ -19,5 +25,14 @@ public class GuardAnimation : MonoBehaviour
         {
             animator.SetBool("IsMoving", true);
         }
+    }
+
+    private void Update()
+    {
+        //Move the animation holder to the correct position.
+        animationHolder.transform.position = enemy.transform.position;
+
+        //If the rotation of the enemy is below 180, keep rotation the same, otherwise flip it.
+        sr.flipX = enemy.transform.rotation.eulerAngles.z > 180;
     }
 }
